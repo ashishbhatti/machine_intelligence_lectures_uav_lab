7f81b7f6912877a1601fa3b588708a07a3e29209
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-07-10 16:18:44 +0530

    Adding Basic Movements of Tello

--- tello_programming/2BasicMovements.py
+++ tello_programming/2BasicMovements.py
@@ -6,10 +6,16 @@ me.connect()              # this takes care of ip connections and communication
 
 print(me.get_battery())
 
+
+# Control the drone
 me.takeoff()
 
 # controlling our drone through speed
-me.send_rc_control(0, 50, 0, 0)                   # forward 50
+me.send_rc_control(0, 50, 0, 0)                   # forward translation 50
+sleep(2)
+me.send_rc_control(30, 0, 0, 0)                   # right translation 30
+sleep(2)
+me.send_rc_control(0, 0, 0, 30)                   # yaw 30
 sleep(2)
 me.send_rc_control(0, 0, 0, 0)                    # stop forward movement
 me.land()
