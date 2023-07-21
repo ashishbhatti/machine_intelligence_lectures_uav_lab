a8830a9787a8d4044fc24cee80ca0de392c90239
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-07-21 17:21:22 +0530

    Beginning Project 2

---
+++ tello_programming/6Mapping.py
@@ -0,0 +1,12 @@
+'''
+Odometry Problem: How to localize the drone in the surroundings?
+
+We will take the velocity information, to find the distance.
+We will also use angular speed, to find the angle of drone,
+or where it is headed. Based on this we will find the x,y coordinates
+of the drone.
+
+So, we will take the distance and angle and we will convert it into
+cartesian coordinates, and plot it into a graph.
+
+'''
