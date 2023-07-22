d3fd65547f5f4c3b745fa9b7e1cf2e2258b4ff1a
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-07-22 17:37:15 +0530

    Explaining Project 2

--- tello_programming/6Mapping.py
+++ tello_programming/6Mapping.py
@@ -9,4 +9,14 @@ of the drone.
 So, we will take the distance and angle and we will convert it into
 cartesian coordinates, and plot it into a graph.
 
+# Translation
+Distance = Speed x Time
+
+# Rotation
+Angle (Heading) = AngularSpeed x Time
+
+# How to calculate when both are pressed? forward and rotation
+We will find coordinates after every unit of time, so we will
+have intermediate positions.
+
 '''
