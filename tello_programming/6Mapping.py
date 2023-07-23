52b96f9d0c6f567974874586905bc8736ec9546c
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-07-23 17:46:08 +0530

    Project 2

--- tello_programming/6Mapping.py
+++ tello_programming/6Mapping.py
@@ -20,3 +20,53 @@ We will find coordinates after every unit of time, so we will
 have intermediate positions.
 
 '''
+
+from djitellopy import tello
+import KeyPressModule as kp
+from time import sleep
+
+# --------- Initialization --------
+kp.init()
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+# ------- Parameters ------------
+fSpeed = 117/10              # Forward Speed in cm/sec (actual speed was 15 cm/s)
+aSpeed = 360/10              # Angular Speed Degrees/s
+interval = 0.25              # time interval to calculate
+
+dInterval = fSpeed * interval
+aInterval = aSpeed * interval
+
+
+
+def getKeyboardInput():
+    # defining params left-right, forward-backward, up-down, yaw-velocity
+    lr, fb, ud, yv  = 0, 0, 0, 0
+    speed = 50
+
+    if kp.getKey("LEFT"): lr = -speed
+    elif kp.getKey("RIGHT"): lr = speed
+
+    if kp.getKey("UP"): fb = speed
+    elif kp.getKey("DOWN"): fb = -speed
+
+    if kp.getKey("w"): ud = speed
+    elif kp.getKey("s"): ud = -speed
+    
+    if kp.getKey("a"): yv = speed
+    elif kp.getKey("d"): yv = -speed
+
+    if kp.getKey("q"): me.land()
+    if kp.getKey("e"): me.takeoff()
+    
+    return [lr, fb, ud, yv]
+
+
+
+while True:
+    vals = getKeyboardInput()
+    me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    print(kp.getKey("s"))
+    sleep(0.05)
