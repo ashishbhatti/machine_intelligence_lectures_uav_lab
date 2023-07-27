be476197922ad1e2655ea01505c335aa2e5fa1f2
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-07-27 18:10:05 +0530

    Project 2

--- tello_programming/6Mapping.py
+++ tello_programming/6Mapping.py
@@ -26,6 +26,7 @@ import KeyPressModule as kp
 from time import sleep
 import numpy as np
 import cv2
+import math
 
 # --------- Initialization --------
 kp.init()
@@ -52,7 +53,7 @@ def getKeyboardInput():
     lr, fb, ud, yv  = 0, 0, 0, 0
     speed = 50
     d = 0
-    global yaw
+    global x,y,yaw, a
 
     if kp.getKey("LEFT"):
         lr = -speed
@@ -85,17 +86,23 @@ def getKeyboardInput():
 
     if kp.getKey("q"): me.land()
     if kp.getKey("e"): me.takeoff()
-    
-    return [lr, fb, ud, yv]
 
-def drawPoints():
-    cv2.circle(img, (x,y), 5, (0,0,255), cv2.FILLED)
+    a += yaw
+    x += int(d*math.cos(math.radians(a)))
+    y += int(d*math.sin(math.radians(a)))
+
+    
+    return [lr, fb, ud, yv, x, y]
+
+def drawPoints(img, points):
+    cv2.circle(img, (points[0],points[1]), 5, (0,0,255), cv2.FILLED)
 
 while True:
     vals = getKeyboardInput()
     me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
 
     img = np.zeros((1000,1000,3), np.uint8)
-    drawPoints()
+    points = vals[4], vals[5]
+    drawPoints(img, points)
     cv2.imshow("Output", img)
     cv2.waitKey(1)
