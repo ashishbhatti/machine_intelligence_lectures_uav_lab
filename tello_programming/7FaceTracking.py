11ecbc1832ce7dffbbf4fde155e3f632718d170e
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-08-04 01:15:17 +0530

    Project 3 Face Tracking

--- tello_programming/7FaceTracking.py
+++ tello_programming/7FaceTracking.py
@@ -35,12 +35,25 @@ def findFace(img):
     faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(imgGray, 1.2, 8)
+    # change scale factor and nearest neighbours for good results
 
     myFaceListC = []                  # to store center points
     myFaceListArea = []              # to store area
     
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
+        cx = x + w // 2
+        cy = y + h // 2
+        area = w*h
+        cv2.circle(img, (cx,cy), 5, (0,255,0), cv2.FILLED)
+        myFaceListC.append([cx,cy])
+        myFaceListArea.append(area)
+
+    if len(myFaceListArea) != 0:
+        i = myFaceListArea.index(max(myFaceListArea))
+        return img, [myFaceListC[i], myFaceListArea[i]]
+    else:
+        return img, [[0,0], 0]
 
 
 # running the webcam for now
@@ -48,6 +61,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     _, img = cap.read()
-    findFace(img)
+    img, info = findFace(img)
+    print("Area", info[1])
     cv2.imshow("Output", img)
     cv2.waitKey(1)
