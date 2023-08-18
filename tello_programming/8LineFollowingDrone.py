7c91d6e43daf793606d6df69f607395685da3404
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-08-18 16:04:13 +0530

    Project-4 Line Follower Drone: Describing the algorithm

--- tello_programming/8LineFollowingDrone.py
+++ tello_programming/8LineFollowingDrone.py
@@ -19,6 +19,7 @@ We have 2^3 = 8 possibilities.
 
 
 How to implement it on drone?
+ROTATION
 1. Camera need to face downwards 
     A mirror infront of camera to make it look downwards.
     A clip is available on thingiverse, to attach mirror. Or you can 3D print it. Or just 
@@ -36,5 +37,21 @@ How to implement it on drone?
     Because we are splitting our image into virtual IR sensors, we can
     split them into as many as we want, say 5.
     But then 2^5 = 32, are too many combinations to handle for a simple problem.
+
+TRANSLATION
+Note in the case of line follower robot, the robot could translate in one direction, 
+and rotation in one. So, to change the position of robot over the line, the robot can only 
+rotate and move forward.
+
+But a drone can translate side to side as well. 
+Imagine a situtation, when the line falls in the right region of the drone camera, 
+and maybe the drone should translate to the side to keep it centered, rather than rotation.
+We have to make sure that the drone is always over the line, that is the line shoulf be in ceter region.
+Then only it will be able to tell, when to rotate.
+
+How to solve this?
+At every step we will try to find the center of the black line. And check where the drone is wrt this center.
+It the drone center is away from line center, we will try to add translation to the drone to match them.
+
 
 '''
