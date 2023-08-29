dd83614eb648d6d9d68214661275e61acbe867a3
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-08-29 13:11:53 +0530

    Project-4 Line Follower Drone

--- tello_programming/8LineFollowerDrone.py
+++ tello_programming/8LineFollowerDrone.py
@@ -64,6 +64,12 @@ import numpy as np
 hsvVals = [0, 0, 117, 179, 22, 219]
 sensors = 3
 thresholdR = 0.2                                           # Rotation threshold 20%
+width, height = 480, 360
+sensitivity = 3                                            # if number is high, less sensitive
+
+weights = [-25, -15, 0, 15, 25]
+curve = 0
+fSpeed = 15
 
 def thresholding(img):
     """
@@ -114,26 +120,45 @@ def getSensorOutput(imgThres, sensors):
             senOut.append(1)
         else:
             senOut.append(0)
-        cv2.imshow(str(x), im)
-
-    print (senOut)
+        # cv2.imshow(str(x), im)
+    # print (senOut)
     return senOut
+
+
+def sendCommands(senOut, cx):
+    global curve
+    # translation
+    lr = (cx - width // 2) // sensitivity
+    lr = int(np.clip(lr, -10, 10))
+    if lr < 2 and lr > -2: lr = 0
+
+    # rotation
+    if   senOut == [1, 0, 0]: curve = weights[0]
+    elif senOut == [1, 1, 0]: curve = weights[1]
+    elif senOut == [0, 1, 0]: curve = weights[2]
+    elif senOut == [0, 1, 1]: curve = weights[3]
+    elif senOut == [0, 0, 1]: curve = weights[4]
+
+    elif senOut == [0, 0, 0]: curve = weights[2]
+    elif senOut == [1, 1, 1]: curve = weights[2]
+    elif senOut == [1, 0, 1]: curve = weights[2]
+
+    # me.send_rc_control(lr, fSpeed, 0, curve)
+
 
 
 
 cap = cv2.VideoCapture(0)
 while True:
     _, img = cap.read()
-    img = cv2.resize(img, (480, 360))
-
+    img = cv2.resize(img, (width, height))
     # because using a mirror, image flipped vertically
     # img = cv2.flip(img, 0)
 
     imgThres = thresholding(img)
     cx = getContours(imgThres, img)                        # For translation
-    senOut = getSensorOutput(imgThres, sensors)
-
-
+    senOut = getSensorOutput(imgThres, sensors)            # For rotation
+    sendCommands(senOut, cx)
 
     cv2.imshow("Output", img)
     cv2.imshow("Path", imgThres)
