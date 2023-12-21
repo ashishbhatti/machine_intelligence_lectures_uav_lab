1c825fd446373a202183fced69244eec04abaa5f
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-12-21 19:07:14 +0530

    added face recognition and bbox rectangle

--- face_recognition/face_recognition_real_time_database/main.py
+++ face_recognition/face_recognition_real_time_database/main.py
@@ -1,5 +1,10 @@
 import cv2
 import os
+import pickle
+
+import face_recognition
+import numpy as np
+import cvzone
 
 # webcam
 cap = cv2.VideoCapture(0)
@@ -11,18 +16,46 @@ imgBackground = cv2.imread('Resources/background.png')
 folderModePath = 'Resources/Modes'
 modePathList = os.listdir(folderModePath)
 imgModeList = []
-
 for path in modePathList:
     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
-
 # print(len(imgModeList))
 
+# load the encodings file
+print("Loading Encode File ...")
+file = open("EncodeFile.p",'rb')
+encodeListKnownWithIds = pickle.load(file)
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encode File Loaded.")
 
 while True:
     success, img = cap.read()
 
+    imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurrFrame = face_recognition.face_locations(imgS)
+    encodeCurrFrame = face_recognition.face_encodings(imgS, facesCurrFrame)
+
     imgBackground[162:162+480, 55:55+640] = img                    # overlay webcam image over background image
     imgBackground[44:44+633, 808:808+414] = imgModeList[3]         # overlay mode images on background image
+
+    for encodeFace, faceLoc in zip(encodeCurrFrame, facesCurrFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print("matches", matches)
+        # print("faceDis", faceDis)
+
+        matchIndex = np.argmin(faceDis)
+        if matches[matchIndex]:
+            print("Known Face Detected")
+            print(studentIds[matchIndex])
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            bbox = 55+x1, 162+y1, x2-x1, y2-y1
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+
+
 
     # cv2.imshow("Webcam", img)
     cv2.imshow("Face Attendance", imgBackground)
