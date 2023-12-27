477313ce08bb7021747137689bdff6402c14037f
ashishbhatti <ashish_kumar1496@yahoo.com>
2023-12-27 13:04:06 +0530

    Added data retrieval from storage.

--- face_recognition/face_recognition_real_time_database/EncodeGenerator.py
+++ face_recognition/face_recognition_real_time_database/EncodeGenerator.py
@@ -2,6 +2,16 @@ import cv2
 import face_recognition
 import pickle
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendancerealtime-271223-default-rtdb.firebaseio.com/",
+    'storageBucket': "faceattendancerealtime-271223.appspot.com"
+})
 
 # importing the known student images
 folderPath = 'Images'
@@ -11,6 +21,12 @@ studentIds = []
 for path in imgPathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     studentIds.append(os.path.splitext(path)[0])
+
+    # upload images to the firebase storage
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
 print(studentIds)
 
 
--- face_recognition/face_recognition_real_time_database/main.py
+++ face_recognition/face_recognition_real_time_database/main.py
@@ -6,6 +6,17 @@ import face_recognition
 import numpy as np
 import cvzone
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendancerealtime-271223-default-rtdb.firebaseio.com/",
+    'storageBucket': "faceattendancerealtime-271223.appspot.com"
+})
+
 # webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -28,6 +39,10 @@ encodeListKnown, studentIds = encodeListKnownWithIds
 # print(studentIds)
 print("Encode File Loaded.")
 
+modeType = 0
+counter = 0
+id = -1
+
 while True:
     success, img = cap.read()
 
@@ -38,7 +53,7 @@ while True:
     encodeCurrFrame = face_recognition.face_encodings(imgS, facesCurrFrame)
 
     imgBackground[162:162+480, 55:55+640] = img                    # overlay webcam image over background image
-    imgBackground[44:44+633, 808:808+414] = imgModeList[3]         # overlay mode images on background image
+    imgBackground[44:44+633, 808:808+414] = imgModeList[0]         # overlay mode images on background image
 
     for encodeFace, faceLoc in zip(encodeCurrFrame, facesCurrFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
@@ -54,8 +69,15 @@ while True:
             y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
             bbox = 55+x1, 162+y1, x2-x1, y2-y1
             imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
-
-
+            id = studentIds[matchIndex]
+            if counter == 0:
+                counter = 1
+                modeType = 1
+    if counter != 0:
+        if counter == 1:
+            studentInfo = db.reference(f'Students/{id}').get()
+            print(studentInfo)
+        counter += 1
 
     # cv2.imshow("Webcam", img)
     cv2.imshow("Face Attendance", imgBackground)
