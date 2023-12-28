8553507a15cb83d4ffb3ac71723f366cb8a7a058
Ashish Kumar <ashish_kumar1496@yahoo.com>
2023-12-28 17:07:47 +0530

    completed the MobileNet-SSD module

--- jetson-nano/real-time-object-detection-jetson-inference/MobileNetSSDModule.py
+++ jetson-nano/real-time-object-detection-jetson-inference/MobileNetSSDModule.py
@@ -12,3 +12,56 @@ $ sudo make install
 $ sudo ldconfig
 
 '''
+
+import jetson_inference
+import jetson_utils
+import cv2
+
+
+class mnSSD():
+    # intializing instance variables
+    def __init__(self, path, threshold):
+        self.path = path
+        self.threshold = threshold
+        self.net = jetson_inference.detectNet(self.path, self.threshold)
+
+    # method to detect
+    def detect(self, img, display=False):
+        imgCuda = jetson_utils.cudaFromNumpy(img)
+        detections = self.net.Detect(imgCuda, overlay="OVERLAY_NONE")   # don't overlay bboxes on the image
+
+        objects = []
+        for d in detections:
+            className = self.net.GetClassDesc(d.ClassID)
+            objects.append([className, d])
+            
+            if display == True:
+                x1,y1,x2,y2 = int(d.Left), int(d.Top), int(d.Right), int(d.Bottom)
+                cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255), 2)
+                cv2.putText(img, className, (x1+5, y1+15), cv2.FONT_HERSHEY_DUPLEX, 0.75, (255,0,255),2) 
+                cv2.putText(img, f'FPS: {int(self.net.GetNetworkFPS())}', (30, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (255,0,0),2) 
+            else: 
+                print((self.net.GetNetworkFPS()))
+        return objects
+
+def main():
+    """
+    Implements object detection based on MobilenetSSD-v2
+    """
+    # webcam
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640)
+    cap.set(4,480)
+
+    myModel = mnSSD("ssd-mobilenet-v2", 0.5)
+
+    while True:
+        success, img = cap.read()
+        objects = myModel.detect(img, False)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
--- jetson-nano/real-time-object-detection-jetson-inference/bare_min_testy.py
+++ jetson-nano/real-time-object-detection-jetson-inference/bare_min_testy.py
@@ -1,3 +1,17 @@
+''' How each detection looks like?
+<detectNet.Detection object>
+   -- Confidence: 0.820312
+   -- ClassID: 62
+   -- Left:    88.5938
+   -- Top:     142.969
+   -- Right:   186.562
+   -- Bottom:  278.672
+   -- Width:   97.9688
+   -- Height:  135.703
+   -- Area:    13294.7
+   -- Center:  (137.578, 210.82)
+'''
+
 import jetson.inference
 import jetson.utils
 import cv2
@@ -18,5 +32,24 @@ cap.set(4,480)
 
 while True:
     success, img = cap.read()
+    # image that comes from opencv is based on numpy format
+    # not recognized by net
+    # converting to cuda format
+    imgCuda = jetson.utils.cudaFromNumpy(img)
+
+    detections = net.Detect(imgCuda)
+
+    # how to display detections? 2 methods
+    # method 1: converting imgCuda to opencv img (easier but inflexible)
+    # img = jetson.utils.cudaToNumpy(imgCuda)
+
+    # method 2: overlaying the detections on opencv image
+    for d in detections:
+        # print(d)
+        x1,y1,x2,y2 = int(d.Left), int(d.Top), int(d.Right), int(d.Bottom)
+        className = net.GetClassDesc(d.ClassID)
+        cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255), 2)
+        cv2.putText(img, className, (x1+5, y1+15), cv2.FONT_HERSHEY_DUPLEX, 0.75, (255,0,255),2)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
